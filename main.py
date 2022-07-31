f6cf24ab03f3da5a1e7a7282f7a9bd633dcd949a
Adam Podoxin <adam.podoxin@gmail.com>
2022-07-30 20:43:38 -0700

    Restructure project to be standalone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from etym import get_etym
-from flask import Flask, request
 from morph import get_morphs, get_root_spliced
 
 
@@ -11,38 +10,18 @@ def build_morpheme(morph, type, etym):
     }
 
 
-# Init app
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "Hello World!"
-
-
-@app.route("/morphemes", methods=["GET"])
-def morphemes():
-    word = request.args["word"]
-
-    morphs = get_morphs(word)
-
+def get_prefixes(root_spliced):
     prefixes = []
-    root_morph = {}
-    suffixes = []
-
-    tree = morphs["tree"]
-    tree_children = tree[0]["children"]
-    root = tree_children[0]["text"]
-    root_spliced = get_root_spliced(root)
-
     for i in range(len(root_spliced) - 1):
         current_morph = root_spliced[i]
         prefixes.append(build_morpheme(current_morph, "bound",
                         get_etym(f"{current_morph}-")))
 
-    root_word = root_spliced[len(root_spliced) - 1]
-    root_morph = build_morpheme(root_word, "free", get_etym(root_word))
+        return prefixes
 
+
+def get_suffixes(tree, tree_children):
+    suffixes = []
     for i in range(1, len(tree_children)):
         current_morph = tree_children[i]["text"]
         suffixes.append(build_morpheme(current_morph, "bound",
@@ -53,13 +32,23 @@ def morphemes():
         suffixes.append(build_morpheme(current_morph, "bound",
                         get_etym(f"-{current_morph}")))
 
+
+def analyze(word):
+    morphs = get_morphs(word)
+
+    tree = morphs["tree"]
+    tree_children = tree[0]["children"]
+    root = tree_children[0]["text"]
+    root_spliced = get_root_spliced(root)
+
+    prefixes = get_prefixes(root_spliced)
+    suffixes = get_suffixes(tree, tree_children)
+
+    root_word = root_spliced[len(root_spliced) - 1]
+    root_morph = build_morpheme(root_word, "free", get_etym(root_word))
+
     return {
         "prefixes": prefixes,
         "root": root_morph,
         "suffixes": suffixes
     }
-
-
-# Run server
-if __name__ == "__main__":
-    app.run(debug=True)

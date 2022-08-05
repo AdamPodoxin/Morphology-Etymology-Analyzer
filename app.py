75d664787e7d2c1ddd4736f7e8780be8cb33c5a1
Adam Podoxin <adam.podoxin@gmail.com>
2022-08-05 14:17:11 -0700

    Disable CORS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from main import analyze
 
 app = Flask(__name__)
@@ -12,4 +12,9 @@ def index():
 @app.route("/analyze", methods=["GET"])
 def analyze_route():
     word = request.args.get("word")
-    return analyze(word)
+    analysis = analyze(word)
+
+    response = jsonify(analysis)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    
+    return response

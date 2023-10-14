7c5f8c39719571535a711097a1a71bb31432669d
Adam Podoxin <adam.podoxin@gmail.com>
2023-10-13 20:18:07 -0700

    Remove etymology part

--- etymology.py
+++ etymology.py
@@ -3,25 +3,26 @@ from bs4 import BeautifulSoup
 
 
 def get_etymology(word: str) -> str:
-    URL = f"https://www.etymonline.com/word/{word}"
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
+	return ""
+    # URL = f"https://www.etymonline.com/word/{word}"
+    # page = requests.get(URL)
+    # soup = BeautifulSoup(page.content, "html.parser")
 
-    try:
-        section = soup.find_all("section", class_="word__defination--2q7ZH")[0]
-        paragraphs = section.findAll("p")
-        first_paragraph = paragraphs[0]
+    # try:
+    #     section = soup.find_all("section", class_="word__defination--2q7ZH")[0]
+    #     paragraphs = section.findAll("p")
+    #     first_paragraph = paragraphs[0]
 
-        i = 0
-        while i < len(paragraphs) and len(first_paragraph.text) == 0:
-            i += 1
-            first_paragraph = paragraphs[i]
+    #     i = 0
+    #     while i < len(paragraphs) and len(first_paragraph.text) == 0:
+    #         i += 1
+    #         first_paragraph = paragraphs[i]
 
-        etymology_text = first_paragraph.text
+    #     etymology_text = first_paragraph.text
 
-        return etymology_text
-    except:
-        if '-' in word:
-            return get_etymology(word.replace('-', ''))
-        else:
-            return ""
+    #     return etymology_text
+    # except:
+    #     if '-' in word:
+    #         return get_etymology(word.replace('-', ''))
+    #     else:
+    #         return ""

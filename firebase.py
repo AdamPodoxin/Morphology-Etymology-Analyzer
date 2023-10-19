0d04c6c1e8a35eea873efcd8c06f1292d4a88654
Adam Podoxin <adam.podoxin@gmail.com>
2023-10-18 21:58:07 -0700

    Update to use dotenv

--- firebase.py
+++ firebase.py
@@ -4,23 +4,23 @@ from firebase_admin import firestore
 
 from google.cloud.firestore_v1.client import Client
 
-from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
 
-
-env_variables = dotenv_values()
+load_dotenv()
 
 cred_values = { \
-	"type": env_variables["type"], \
-	"project_id": env_variables["project_id"], \
-	"private_key_id": env_variables["private_key_id"], \
-	"private_key": env_variables["private_key"].replace('\\n', '\n'), \
-	"client_email": env_variables["client_email"], \
-	"client_id": env_variables["client_id"], \
-	"auth_uri": env_variables["auth_uri"], \
-	"token_uri": env_variables["token_uri"], \
-	"auth_provider_x509_cert_url": env_variables["auth_provider_x509_cert_url"], \
-	"client_x509_cert_url": env_variables["client_x509_cert_url"], \
-	"universe_domain": env_variables["universe_domain"], \
+	"type": os.getenv("type"), \
+	"project_id": os.getenv("project_id"), \
+	"private_key_id": os.getenv("private_key_id"), \
+	"private_key": os.getenv("private_key").replace('\\n', '\n'), \
+	"client_email": os.getenv("client_email"), \
+	"client_id": os.getenv("client_id"), \
+	"auth_uri": os.getenv("auth_uri"), \
+	"token_uri": os.getenv("token_uri"), \
+	"auth_provider_x509_cert_url": os.getenv("auth_provider_x509_cert_url"), \
+	"client_x509_cert_url": os.getenv("client_x509_cert_url"), \
+	"universe_domain": os.getenv("universe_domain"), \
 }
 
 cred = Certificate(cred_values)
